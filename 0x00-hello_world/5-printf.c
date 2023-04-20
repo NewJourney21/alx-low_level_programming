fa28a4a8696e3210c7642ff5ce554d26b18b496a
NewJourney21 <portiajayden21@gmail.com>
2023-04-20 09:27:11 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,19 +1,10 @@
 #include <stdio.h>
 /**
- * main -A program that prints the size of various computer types
- * Return 0 (Success)
+ * main -A program that prints a line using the printf function
+ * Return: 0 (Success)
  */
 int main(void)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float f;
-
-printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
-printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
-printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
-printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
-return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);	
+}	
