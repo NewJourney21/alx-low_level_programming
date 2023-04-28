62e4b2b77180b3da80f500e05bd73768f1a0fda8
NewJourney21 <portiajayden21@gmail.com>
2023-04-28 08:51:37 -0700

    10-print

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,9 +7,9 @@
 void print_triangle(int size)
 {
 if (size <= 0)
-
+{
 _putchar('\n');
-
+}
 else
 {
 int i, j;
@@ -24,8 +24,9 @@ _putchar(' ');
 for (j = 1; j <= i; j++)
 {
 _putchar('#');
+}
+
 _putchar('\n');
-return(0);	
-
-
-
+}
+}
+}
