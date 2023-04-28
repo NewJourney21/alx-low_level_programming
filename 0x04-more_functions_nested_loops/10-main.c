7e266d5559d8872de0a3ddf087743bf048ff0186
NewJourney21 <portiajayden21@gmail.com>
2023-04-28 07:11:55 -0700

    10-main.c

---
+++ 0x04-more_functions_nested_loops/10-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+print_triangle(2);
+rint_triangle(10);
+print_triangle(1);
+print_triangle(0);
+return (0);
+}
